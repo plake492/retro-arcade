ec86b3839bfab4ed4301d9ebecf69c4b816229ff
plake <plake492@gmail.com>
2019-08-31 10:41:13 -0400

    added more games to datat object

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require ("express-handlebars");
 const app = express();
-
+ 
 const PORT = process.env.PORT || 3001;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
